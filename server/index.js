68c3221bc0a1f0de374a3ab7600eb1d56f955b19
dsubotic <dragan.subi7@gmail.com>
2023-04-24 20:46:49 +0200

    add cors handling in server requests

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  next()
+})
+
 app.post('/checkout', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
